6ce2fccceb685e6a07b2a8a89ceb73a517f6d70e
Christiano <christianodvids@gmail.com>
2024-10-14 16:14:46 -0300

    feat: add videomakerData

---
+++ src/data/videomakerData.ts
@@ -0,0 +1,39 @@
+export interface VideomakerData {
+  title: string;
+  video: string;
+  alt: string;
+  href: string;
+}
+
+export const videomakerData: VideomakerData[] = [
+  {
+    title: "Volta às aulas",
+    video: "/videos/video001.mp4",
+    alt: "Video do Volta às aulas",
+    href: "https://www.instagram.com/reel/C-a4yyZRw_w/?igsh=MXM0NWd5ZHMwamRqMg==",
+  },
+  {
+    title: "Feira de profissões",
+    video: "/videos/video002.mp4",
+    alt: "Vídeo da Feira de Profissões",
+    href: "https://www.instagram.com/reel/C-YG10fRyih/?igsh=MTQwaTNoczhiaGdyMg==",
+  },
+  {
+    title: "4º dia universo tuiuti",
+    video: "/videos/video003.mp4",
+    alt: "Vídeo do 4º dia universo tuiuti",
+    href: "https://www.instagram.com/reel/C_bZhodxBSH/?igsh=Znp6cm9mMmNxazl4",
+  },
+  {
+    title: "Vem aí... universo tuiuti",
+    video: "/videos/video004.mp4",
+    alt: "Vídeo do convite do universo tuiuti",
+    href: "https://www.instagram.com/reel/C-uuL8KRI9s/?igsh=MWZsaHJ1cHlyYzJ3bA==",
+  },
+  {
+    title: "Vote nelas paraná",
+    video: "/videos/video005.mp4",
+    alt: "Vídeo do vote nelas paraná",
+    href: "https://www.instagram.com/reel/C6W97ZeOH3S/?igsh=MTM4MHR4bWI1aDJoZw==",
+  },
+];
