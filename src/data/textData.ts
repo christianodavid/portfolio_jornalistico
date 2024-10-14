b19b708a73c2266cafc588e39312bad1840eb15e
Christiano <christianodvids@gmail.com>
2024-10-14 16:08:51 -0300

    feat: add textData

---
+++ src/data/textData.ts
@@ -0,0 +1,40 @@
+export interface TextData {
+  heading: string;
+  subheading: string;
+  paragraph0: string;
+  paragraph1: string;
+  href: string;
+}
+
+export const textData: TextData[] = [
+  {
+    heading: "Mais de 40% da população paranaense está inadimplente",
+    subheading:
+      "De acordo com levantamento da Serasa, número de endividados cresce principalmente entre jovens e adolescentes no Estado",
+    paragraph0:
+      "O endividamento no Brasil representa um ponto de atenção para a economia. A empresa Serviços de Assessoria S.A (Serasa) realizou uma pesquisa em junho de 2023, que mostra que 42% da população paranaense não conseguiu pagar as contas dentro do prazo de vencimento, incluindo jovens e adolescentes.",
+    paragraph1:
+      "Além disso, há uma parcela de brasileiros endividados, que tem parte da sua renda total mensal comprometida com um pagamento futuro, como mostra um levantamento realizado pela Confederação Nacional do Comércio (CNC), chegando a 78,5% da população neste mesmo mês.",
+    href: "https://medium.com/@gabrieladanieljo/mais-de-40-da-popula%C3%A7%C3%A3o-paranaense-est%C3%A1-inadimplente-98b52ad2cd25",
+  },
+  {
+    heading: "Era do Hiperconsumismo e a influência das Redes Sociais",
+    subheading:
+      "O fenômeno está moldando as decisões de compra de muitos adolescentes, levando a um ciclo de compras impulsivas e arrependimentos",
+    paragraph0:
+      "Definido como o ato de consumir de forma exagerada e compulsiva na busca de felicidade ou prazer através da aquisição de objetos e serviços, o termo ‘Hiperconsumismo’ tem ganhado força entre os jovens, principalmente pela exposição constante a publicidades nas redes sociais.",
+    paragraph1:
+      "Segundo uma pesquisa feita pela agência de comunicação MARCO, 73% dos brasileiros disseram já ter comprado algum produto ou serviço baseado na indicação de alguma personalidade digital. Esses números evidenciam o poder do marketing digital em moldar comportamentos de compra, especialmente entre os jovens, que são mais suscetíveis às tendências e influências digitais.",
+    href: "https://medium.com/@gabrieladanieljo/era-do-hiperconsumismo-e-a-influ%C3%AAncia-das-redes-sociais-2c15914785e7",
+  },
+  {
+    heading: "Preço das passagens aéreas no Brasil sobe 13,47% em setembro",
+    subheading:
+      "Reajuste dos combustíveis e a pandemia covid-19 são alguns dos motivos daumento",
+    paragraph0:
+      "Viajar de avião já não é mais uma opção tão viável para alguns brasileiros. O aumento do preço das passagens aéreas contou com um reajuste significativo em setembro desse ano, de 13,47%, em relação ao mês anterior. De acordo com dados do Índice de Preços ao Consumidor Amplo (IPCA), esse número é a segunda maior variação mensal e o segundo com maior impacto no país, após uma queda de 11,69% em agosto.",
+    paragraph1:
+      "Com o aumento, os brasileiros têm buscado por alternativas, como viagens de ônibus e a procura por destinos onde o Real é mais valorizado. Segundo a CEO da Escape Turismo, Giordana Piazzetta Giacomazzi, os clientes têm buscado por viagens mais baratas ao bolso. “Neste ano notamos uma procura muito grande por destinos como a Argentina, que pela desvalorização da moeda, se tornou um destino atrativo para brasileiros. Além de ser um destino barato, partindo de lugares como o Rio Grande do Sul, pode-se ir de carro ou de ônibus.”",
+    href: "https://medium.com/@gabrieladanieljo/pre%C3%A7o-das-passagens-a%C3%A9reas-no-brasil-sobe-13-47-em-setembro-9f8e3362b814",
+  },
+];
