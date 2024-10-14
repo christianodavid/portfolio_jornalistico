68d5de2d4aa4edbadf13a88794ec93040466ceef
Christiano <christianodvids@gmail.com>
2024-10-14 16:22:41 -0300

    feat: add galleryData

---
+++ src/data/galleryData.ts
@@ -0,0 +1,33 @@
+export interface GalleryData {
+  image: string;
+}
+
+export const galleryData: GalleryData[] = [
+  {
+    image: "/images/carrossel1000.png",
+  },
+  {
+    image: "/images/carrossel1001.png",
+  },
+  {
+    image: "/images/carrossel1002.png",
+  },
+  {
+    image: "/images/carrossel1003.png",
+  },
+  {
+    image: "/images/carrossel2000.png",
+  },
+  {
+    image: "/images/carrossel2001.png",
+  },
+  {
+    image: "/images/carrossel2002.png",
+  },
+  {
+    image: "/images/carrossel2003.png",
+  },
+  {
+    image: "/images/carrossel2004.png",
+  },
+];
