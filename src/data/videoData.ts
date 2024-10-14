17e67cd8c113952e09c0269dc713c4cfeaf1f777
Christiano <christianodvids@gmail.com>
2024-10-14 16:19:20 -0300

    feat: add videoData

---
+++ src/data/videoData.ts
@@ -0,0 +1,21 @@
+export interface VideoData {
+  heading: string;
+  href: string;
+  alt: string;
+  link: string;
+}
+
+export const videoData: VideoData[] = [
+  {
+    heading: "Curta-metragem",
+    href: "/videos/entrelinhas.mp4",
+    alt: "Video curta-metragem Entre Linhas",
+    link: "https://www.youtube.com/watch?v=R074c8QcALE",
+  },
+  {
+    heading: "Reportagem",
+    href: "/videos/reportagem.mp4",
+    alt: "Video de reportagem",
+    link: "https://www.linkedin.com/posts/gabrielasdaniel_utp-tuiuti-jornalismo-activity-7066160628813508608-CW5A/?utm_source=share&utm_medium=member_desktop",
+  },
+];
